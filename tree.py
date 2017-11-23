e0abab9fa7df4dbb4775f91fafa0f3c9e15ebe45
Hisairnessag3 <austinglugla@gmail.com>
2017-11-23 00:25:50 -0500

    Update tree.py

--- tree.py
+++ tree.py
@@ -9,9 +9,6 @@ import graphviz
 
 df = pd.read_csv('mushroom2.csv')
 
-
-enc = LabelEncoder()
-
 #defining attributes
 df = df[['class','cap-shape','cap-surface','cap-color','bruises','odor','gill-attachment','gill-spacing','gill-size','gill-color','stalk-shape','stalk-root','stalk-surface-above-ring','stalk-surface-below-ring','stalk-color-above-ring','stalk-color-below-ring','veil-type','veil-color','ring-number','ring-type','spore-print-color','population','habitat']]
 
